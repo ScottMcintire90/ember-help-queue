311ad16a7baab15f189f7da795d666c9ef597a3c
alex hagge and scott mcintire <alexhagge128@gmail.com>
2016-06-13 13:44:07 -0700

    modified secondselapsed display

--- app/components/ticket-tile.js
+++ app/components/ticket-tile.js
@@ -5,6 +5,10 @@ export default Ember.Component.extend({
 
   timeElapsed: Ember.computed('ticket', function(){
     var secondsElapsed = (moment().unix() - this.get('ticket.date')) / 60;
-      return secondsElapsed.toPrecision('2');
+    if (secondsElapsed.toPrecision(2) > 0 && secondsElapsed.toPrecision(2) < 10) {
+      return secondsElapsed.toPrecision(1);
+    } else {
+      return secondsElapsed.toPrecision(2);
+    }
   })
 });
